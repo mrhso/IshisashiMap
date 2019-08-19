dcafb48f8b76b1c9cbb1b870b87085ed6ad9e072
mrhso <liu.songquan@live.cn>
2019-08-19 18:05:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ const bd_gcj = prcoords.bd_gcj;
 const wgs_bd = (bd, checkChina = true) => gcj_bd(wgs_gcj(bd, checkChina));
 const bd_wgs = (bd, checkChina = true) => gcj_wgs(bd_gcj(bd), checkChina);
 
+const __bored__ = (fwd, rev) => {
+    const PRC_EPS = 1e-5;
+    const _coord_diff = (a, b) => ({ lat: a.lat - b.lat, lon: a.lon - b.lon });
+
+    // maxTimes 表示最大迭代次数
+    return (heck, checkChina = true, maxTimes = 10) => {
+        let curr = rev(heck, checkChina);
+        let diff = { lat: Infinity, lon: Infinity };
+
+        // Wait till we hit fixed point or get bored
+        let i = 0;
+        while (Math.max(Math.abs(diff.lat), Math.abs(diff.lon)) > PRC_EPS && i++ < maxTimes) {
+            diff = _coord_diff(fwd(curr, checkChina), heck);
+            curr = _coord_diff(curr, diff);
+        };
+
+        return curr;
+    };
+};
+
 const gcj_wgs_bored = prcoords.__bored__(wgs_gcj, gcj_wgs);
 const bd_gcj_bored = prcoords.__bored__(gcj_bd, bd_gcj);
 const bd_wgs_bored = prcoords.__bored__(wgs_bd, bd_wgs);
@@ -22,7 +42,7 @@ const bd_wgs_bored = prcoords.__bored__(wgs_bd, bd_wgs);
 // 坐标转换精度测试
 // 每个 Array 中 [0] 表示转换后的纬度，[1] 表示转换后的经度，[2] 表示转换前后的距离（米），[3] 表示来回转换与原坐标的距离（米）
 // 其中 [3] 可以反映精度
-const deltaTest = (coord, bored = true) => {
+const deltaTest = (coord, bored = true, maxTimes = 10) => {
     const handle = (fwd, rev) => {
         let result_fwd = fwd(coord, false);
         let result_rev = rev(result_fwd, false);
@@ -89,6 +109,7 @@ let expts = {
     bd_gcj,
     wgs_bd,
     bd_wgs,
+    __bored__,
     gcj_wgs_bored,
     bd_gcj_bored,
     bd_wgs_bored,
