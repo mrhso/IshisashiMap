7ba69e300bf0d9083f4fc5077a7531fc3f364a35
mrhso <liu.songquan@live.cn>
2019-09-30 22:34:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const https = require('https');
 
 const isNumber = (arg) => Object.prototype.toString.call(arg) === '[object Number]';
 const round = (num, pow) => isNumber(pow) ? Math.sign(num) * Math.round(Math.abs(num) * Number(`1e${pow}`)) / Number(`1e${pow}`) : num;
-const coordRound = (coord, pow) => ({ lat: round(coord.lat, pow), lon: round(coord.lon, pow) });
+const coordsRound = (coords, pow) => ({ lat: round(coords.lat, pow), lon: round(coords.lon, pow) });
 
-const isInBaidu = (coord) => insane.isInBaidu(coord.lat, coord.lon);
-const isInGoogle = (coord) => insane.isInGoogle(coord.lat, coord.lon);
+const isInBaidu = (coords) => insane.isInBaidu(coords.lat, coords.lon);
+const isInGoogle = (coords) => insane.isInGoogle(coords.lat, coords.lon);
 
 const wgs_gcj = (wgs, checkChina = true) => checkChina && !isInGoogle(wgs) ? (console.warn(`Non-Chinese coords found, returning as-is: (${wgs.lat}, ${wgs.lon})`), wgs) : prcoords.wgs_gcj(wgs, false);
 const gcj_wgs = (gcj, checkChina = true) => checkChina && !isInGoogle(gcj) ? (console.warn(`Non-Chinese coords found, returning as-is: (${gcj.lat}, ${gcj.lon})`), gcj) : prcoords.gcj_wgs(gcj, false);
@@ -65,15 +65,15 @@ const __bored__ = (fwd, rev) => {
             };
             return 0;
         };
-        const coordDigit = (coord) => Math.max(getDigit(coord.lat), getDigit(coord.lon));
+        const coordsDigit = (coords) => Math.max(getDigit(coords.lat), getDigit(coords.lon));
         let pre = minDiffCurr;
         diff = minDiff;
         curr = minDiffCurr;
-        let digit = coordDigit(pre);
+        let digit = coordsDigit(pre);
         let minDigit = digit;
         i = 0;
         while (i++ < digit) {
-            curr = coordRound(pre, i);
+            curr = coordsRound(pre, i);
             diff = _coord_diff(fwd(curr, false), heck);
             if (Math.max(Math.abs(diff.lat), Math.abs(diff.lon)) === Math.max(Math.abs(minDiff.lat), Math.abs(minDiff.lon)) && i < minDigit || Math.max(Math.abs(diff.lat), Math.abs(diff.lon)) < Math.max(Math.abs(minDiff.lat), Math.abs(minDiff.lon))) {
                 minDiff = diff;
@@ -93,15 +93,15 @@ const bd_wgs_bored = __bored__(wgs_bd, bd_wgs);
 // 坐标转换精度测试
 // 每个 Array 中 [0] 表示转换后的坐标，[1] 表示来回转换后的坐标，[2] 表示转换前后的距离（米），[3] 表示来回转换与原坐标的距离（米）
 // 其中 [3] 可以反映精度
-const deltaTest = (coord, bored = true, eps = Number.EPSILON, maxTimes = 20, inputRound, outputRound) => {
-    coord = coordRound(coord, inputRound);
+const deltaTest = (coords, bored = true, eps = Number.EPSILON, maxTimes = 20, inputRound, outputRound) => {
+    coords = coordsRound(coords, inputRound);
     const handle = (fwd, rev) => {
-        let result_fwd = coordRound(fwd(coord, false, eps, maxTimes), outputRound);
-        let result_rev = coordRound(rev(result_fwd, false, eps, maxTimes), inputRound);
-        return [result_fwd, result_rev, prcoords.distance(coord, result_fwd), prcoords.distance(coord, result_rev)];
+        let result_fwd = coordsRound(fwd(coords, false, eps, maxTimes), outputRound);
+        let result_rev = coordsRound(rev(result_fwd, false, eps, maxTimes), inputRound);
+        return [result_fwd, result_rev, prcoords.distance(coords, result_fwd), prcoords.distance(coords, result_rev)];
     };
     return {
-        raw: [coord, coord, 0, 0],
+        raw: [coords, coords, 0, 0],
         wgs_gcj: handle(wgs_gcj, bored ? gcj_wgs_bored : gcj_wgs),
         wgs_bd: handle(wgs_bd, bored ? bd_wgs_bored : bd_wgs),
         gcj_wgs: handle(bored ? gcj_wgs_bored : gcj_wgs, wgs_gcj),
@@ -112,16 +112,16 @@ const deltaTest = (coord, bored = true, eps = Number.EPSILON, maxTimes = 20, inp
 };
 
 // OLC 转坐标
-const olc2coord = (olc, near) => {
+const olc2coords = (olc, near) => {
     let fullOLC = new OpenLocationCode().recoverNearest(olc, near.lat, near.lon);
-    let coord = new OpenLocationCode().decode(fullOLC);
-    return { lat: coord.latitudeCenter, lon: coord.longitudeCenter };
+    let coords = new OpenLocationCode().decode(fullOLC);
+    return { lat: coords.latitudeCenter, lon: coords.longitudeCenter };
 };
 
 // 百度地图坐标转换 API
 // 用于测试 PRCoords 的算法，不建议直接使用
-const baiduGeoconv = (coord, from, to, ak, callback) => {
-    https.get(new URL(`https://api.map.baidu.com/geoconv/v1/?coords=${coord.lon},${coord.lat}&from=${from}&to=${to}&ak=${ak}`), (res) => {
+const baiduGeoconv = (coords, from, to, ak, callback) => {
+    https.get(new URL(`https://api.map.baidu.com/geoconv/v1/?coords=${coords.lon},${coords.lat}&from=${from}&to=${to}&ak=${ak}`), (res) => {
         let chunks = [];
         res.on('data', (chunk) => {
             chunks.push(chunk);
@@ -137,8 +137,8 @@ const baiduGeoconv = (coord, from, to, ak, callback) => {
 
 // 高德地图坐标转换 API
 // 用于测试 PRCoords 的算法，不建议直接使用
-const amapConvert = (coord, coordsys, key, callback) => {
-    https.get(new URL(`https://restapi.amap.com/v3/assistant/coordinate/convert?locations=${coord.lon},${coord.lat}&coordsys=${coordsys}&key=${key}`), (res) => {
+const amapConvert = (coords, coordsys, key, callback) => {
+    https.get(new URL(`https://restapi.amap.com/v3/assistant/coordinate/convert?locations=${coords.lon},${coords.lat}&coordsys=${coordsys}&key=${key}`), (res) => {
         let chunks = [];
         res.on('data', (chunk) => {
             chunks.push(chunk);
@@ -152,11 +152,11 @@ const amapConvert = (coord, coordsys, key, callback) => {
     });
 };
 
-const lonlat2webmct = (coord) => ({ x: (Math.PI / 180) * 6378137 * coord.lon, y: 6378137 * Math.log(Math.tan(Math.PI / 4 + (Math.PI / 180) * coord.lat / 2)) });
-const webmct2lonlat = (coord) => ({ lat: (180 / Math.PI) * 2 * Math.atan(Math.exp(coord.y / 6378137)) - 90, lon: (180 / Math.PI) * coord.x / 6378137 });
+const latlon2webmct = (coords) => ({ x: (Math.PI / 180) * 6378137 * coords.lon, y: 6378137 * Math.log(Math.tan(Math.PI / 4 + (Math.PI / 180) * coords.lat / 2)) });
+const webmct2latlon = (coords) => ({ lat: (180 / Math.PI) * 2 * Math.atan(Math.exp(coords.y / 6378137)) - 90, lon: (180 / Math.PI) * coords.x / 6378137 });
 
 module.exports = {
-    coordRound,
+    coordsRound,
     wgs_gcj,
     gcj_wgs,
     gcj_bd,
@@ -168,11 +168,11 @@ module.exports = {
     bd_gcj_bored,
     bd_wgs_bored,
     deltaTest,
-    olc2coord,
+    olc2coords,
     baiduGeoconv,
     amapConvert,
-    lonlat2webmct,
-    webmct2lonlat,
+    latlon2webmct,
+    webmct2latlon,
     isInBaidu,
     isInGoogle,
 };
